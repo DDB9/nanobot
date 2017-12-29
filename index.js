e24ea019405804c9e092b2ac23e3529f0b2f419f
Daan de Bruijn <32847619+NeonGekko@users.noreply.github.com>
2017-12-29 13:23:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const discord = require('discord.js');
 
-// Token to log into the server
-// Tokens generated at https://discordapp.com/developers/applications/me/395227173788581888
-var token = "token here";
+// Token to log into the server.
+// Tokens generated at: https://discordapp.com/developers/applications/me/395227173788581888
+var token = "Mzk1MjI3MTczNzg4NTgxODg4.DScOMw.0DR0oIjF61dvnOaRdWJAB7CPaEU";
 
 const client = new discord.Client();
 
@@ -25,13 +25,32 @@ client.on ("message", (message) => {
         message.reply(randomIndex[randomGreeting]);
 
     }
-    // Bot returns a random food image if the user asks for it (More food images will come later).
+    // Bot returns a goodbye message if someone says it goodbye.
+    if (message.content.startsWith (prefix + 'cya')) {
+        message.reply ('See you later, buddy!');
+
+    }
+    // Bot returns Username + discriminator and ID of author.
+    if (message.content.startsWith (prefix + 'getid')) {
+        message.channel.send ("Username: " + message.author.username + "#" + message.author.discriminator);
+        message.channel.send ("User ID: " + message.author.id);
+
+    }
+    // But returns a picture of food with a user asks for it.
     if (message.content.startsWith (prefix + 'food')) {
         var randomFood = ['./images/food1.jpg', './images/food2.png', './images/food3.jpg'];
         let foodMixer = Math.floor(Math.random() * randomFood.length);
         message.channel.send ('Gordon Ramsey would be proud', {files: [randomFood[foodMixer]]});
 
     }
+    // Shows credits, might remove later.
+    if (message.content.startsWith (prefix + "credits")) {
+        message.channel.send ('Nanobot made by NeonGekko. Profile picture by Ketoleperouch.');
+
+    }
+
+
 });
 
+// Logging into the server...
 client.login(token);
